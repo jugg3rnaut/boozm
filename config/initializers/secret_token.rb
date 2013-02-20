0f439bba95e10bafc78969a9d2d70e113010db19
Arit Mondal <aritmondal@gmail.com>
2013-02-20 17:37:42 +0530

    generated rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boozm::Application.config.secret_token = 'dc7ef65dfbf45aff0aeb19931be1e3487334f950f18c1f078afb0609525f109495c73b81fbacd4c6ba8928565517f5188ab29a8ef3f75a84e4bdbd1156a55fbc'
